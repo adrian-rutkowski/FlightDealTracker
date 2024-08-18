00d33243a20ed9b654b02982c7bc9f7f8b413c96
Adrian Rutkowski <adrnrtkvski@gmail.com>
2024-08-18 11:15:06 +0200

    fixed issue with checking either azair or kiwi, instead of both for non-kiwi cases

--- src/logger.py
+++ src/logger.py
@@ -5,11 +5,13 @@ from datetime import datetime
 class Logger:
     def __init__(self):
         log_level = logging.INFO
-        log_filename = f'logs/output_{datetime.now().strftime("%d-%m-%Y")}.log'
+        log_filename = f'logs/output_{datetime.now().strftime("%Y-%m-%d")}.log'
 
         # Configure logging
-        logging.basicConfig(filename=log_filename, level=log_level,
-                            format='%(levelname)s - %(message)s')
+        logging.basicConfig(filename=log_filename,
+                            level=log_level,
+                            format='%(asctime)s - %(levelname)s - %(message)s',
+                            datefmt='%H:%M:%S')
 
     def log(self, message, level=logging.INFO):
         # Log a message with the specified level
--- src/main.py
+++ src/main.py
@@ -8,9 +8,8 @@ fm = FlightsManager()
 def check_deals():
     destinations = dm.get_destinations_data()
     for destination in destinations:
-        if destination.only_kiwi:
-            fm.check_kiwi(destination=destination)
-        else:
+        fm.check_kiwi(destination=destination)
+        if not destination.only_kiwi:
             fm.check_azair(destination=destination)
 
 
